f34389c1c5e1889860278fe74c8077e7b344e70e
ARekikGT <134981681+ARekikGT@users.noreply.github.com>
2023-10-04 00:11:53 +0100

    Add files via upload

---
+++ essai1.js
@@ -0,0 +1,48 @@
+var pass = document.getElementById("password");
+var msg = document.getElementById("message");
+var strength = document.getElementById("strength");
+var arrow = document.querySelector("button[type='submit']");
+
+
+
+  togglePassword.addEventListener('click', function (e) {
+    // toggle the type attribute
+    const type = password.getAttribute('type') === 'password' ? 'text' : 'password';
+    password.setAttribute('type', type);
+    // toggle the eye slash icon
+    this.classList.toggle('fa-eye-slash');
+});
+
+arrow.addEventListener("click", function() {
+    if (pass.value.length === 0) {
+        alert("Tip💡: Add UPPERCASE, lowercase, symbols, letters for more secure passwords");
+    } else if (pass.value.length < 4) {
+        alert("🙅🏻‍♀️Password seems to be weak, Try more secure passwords.");
+    } else if (pass.value.length >= 6 && pass.value.length < 12) {
+        alert("📈Password seems to be medium, update it to be more secure.");
+    } else if (pass.value.length >= 12) {
+        alert("✅Password updated");
+    }
+});
+
+pass.addEventListener("input", () => {
+    if (pass.value.length > 0) {
+        msg.style.display = "block";
+    } else {
+        msg.style.display = "none";
+    }
+
+    if (pass.value.length < 4) {
+        strength.innerHTML = "Weak";
+        pass.style.borderColor="#ff5925";
+        msg.style.color="#ff5925";
+    } else if (pass.value.length >= 6 && pass.value.length < 12) {
+        strength.innerHTML = "Medium";
+        pass.style.borderColor="yellow";
+        msg.style.color="yellow";
+    } else if (pass.value.length >= 12) {
+        strength.innerHTML = "Strong";
+        pass.style.borderColor="#26d730";
+        msg.style.color="#26d730";
+    }
+});
